a95132ea3d3f94a357fe5b47a5ef7cddc5e00c6b
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-11-25 23:53:23 -0500

    Refactr header

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,13 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
 })
 export class HeaderComponent {
+
+  @Input() isCollapsed = false;
   @Output() toggleSidebar = new EventEmitter<void>();
+
+  hovering = false;
 
   onToggleSidebar() {
     this.toggleSidebar.emit();
