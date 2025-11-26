122cf9d3af64f563727b7b9c8387c371851fc7fc
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-11-25 23:52:28 -0500

    update shared.table for new design

--- src/app/shared/table/shared-table.ts
+++ src/app/shared/table/shared-table.ts
@@ -1,37 +1,31 @@
-import { Component, Input, Output, EventEmitter , OnChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-shared-table',
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './shared-table.html',
-  styleUrls: ['./shared-table.scss'],
-  imports:[CommonModule, MatTableModule, MatProgressSpinnerModule, MatIconModule, MatButtonModule]
+  styleUrls: ['./shared-table.scss']
 })
-export class SharedTableComponent<T> implements OnChanges {
-  @Input() dataSource: T[] = [];
+export class SharedTableComponent {
+  @Input() dataSource: any[] = [];
   @Input() displayedColumns: string[] = [];
-  @Input() loading = false;
-  @Input() errorMessage: string | null = null;
+  @Input() loading: boolean = false;
+  @Input() errorMessage: string = '';
 
-  @Output() edit = new EventEmitter<T>();
-  @Output() view = new EventEmitter<T>();
+  @Output() edit = new EventEmitter<any>();
+  @Output() view = new EventEmitter<any>();
   @Output() delete = new EventEmitter<number>();
 
-  fullColumns: string[] = [];
-
-  ngOnChanges(): void {
-    this.fullColumns = [...this.displayedColumns, 'acciones'];
-  }
-
-  onEdit(row: T) {
+  onEdit(row: any) {
     this.edit.emit(row);
   }
 
-  onView(row: T) {
+  onView(row: any) {
     this.view.emit(row);
   }
 
