4399e97f67243dc39a0632e92b8c54b1469971d9
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-12-04 23:50:36 -0500

    update appointment model

--- src/app/models/appointment.ts
+++ src/app/models/appointment.ts
@@ -1,14 +1,9 @@
 export interface Appointment {
   id: number;
-  fecha_agendamiento: string;
+  id_paciente: number;         // FK → Patient.id
+  fecha_agendamiento: Date;
   numero_sesion: number;
-  recordatorio: boolean;
-  id_estado_citas: number;
   motivo: string;
   id_profesional: number;
   id_paquetes: number;
-
-  estado?: { id: number; nombre: string };
-  profesional?: { id: number; nombre: string };
-  paquete?: { id: number };
 }
