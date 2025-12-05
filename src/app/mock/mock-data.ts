5779755e15a98a32508145f87a0e0b5df5fde4e7
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-12-05 15:03:35 -0500

    delete azure for new deploy

---
+++ src/app/mock/mock-data.ts
@@ -0,0 +1,11 @@
+export const PROFESIONALES = [
+  { id: 1, nombre: "Dra. Luiza – Fisioterapia" },
+  { id: 2, nombre: "Dr. Edison – Fisioterapia" },
+];
+
+export const PAQUETES = [
+  { id: 1, nombre: "Paquete 10 sesiones – Fisioterapia" },
+  { id: 2, nombre: "Paquete 5 sesiones – Psicología" },
+  { id: 3, nombre: "Paquete 8 sesiones – Terapia Ocupacional" },
+  { id: 4, nombre: "Paquete Medicina General – 3 Controles" }
+];
